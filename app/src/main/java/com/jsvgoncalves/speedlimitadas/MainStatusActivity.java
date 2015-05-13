0225dbdbf552829d0b7a87ba697edf71fef83889
João Gonçalves <jsvgoncalves@gmail.com>
2015-05-13 19:16:07 +0200

    General: Add Status Activity.
    * Adds full main activity
    * Adds 50kmph image resource
    
    Signed-off-by: Joao Goncalves <jsvgoncalves@gmail.com>

--- app/src/main/java/com/jsvgoncalves/speedlimitadas/MainStatusActivity.java
+++ app/src/main/java/com/jsvgoncalves/speedlimitadas/MainStatusActivity.java
@@ -1,34 +1,23 @@
 package com.jsvgoncalves.speedlimitadas;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class EntryActivity extends ActionBarActivity {
+public class MainStatusActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_entry);
-
-        // Check things and then start main
-        startMainActivity();
-    }
-
-    private void startMainActivity() {
-        Intent intent = new Intent(this, MainStatusActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_main_status);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_entry, menu);
+        getMenuInflater().inflate(R.menu.menu_main_status, menu);
         return true;
     }
 
